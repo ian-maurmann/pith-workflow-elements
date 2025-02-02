814173f88a1a0359fac29d002b2bba405440fd4e
Ian Maurmann <ikmxx@yahoo.com>
2025-02-01 18:41:28 -0700

    Add CORS Handler

---
+++ src/base-workflow-elements/PithCorsHandler.php
@@ -0,0 +1,36 @@
+<?php
+# ===================================================================
+# Copyright (c) 2008-2025 Ian K Maurmann. The Pith Framework is
+# provided under the terms of the Mozilla Public License, v. 2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ===================================================================
+
+
+/**
+ * Pith CORS Handler (extend)
+ * --------------------------
+ *
+ *
+ * @noinspection PhpUnused - Ignore, will be extended.
+ */
+
+
+declare(strict_types=1);
+
+
+namespace Pith\Workflow;
+
+
+/**
+ * Class PithCorsHandler
+ */
+class PithCorsHandler
+{
+    public function handleCors()
+    {
+        // Do nothing for now
+    }
+}
